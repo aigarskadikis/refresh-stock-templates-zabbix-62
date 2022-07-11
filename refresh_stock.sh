6dc792c02948ba66e769bfc8bca98255a6e65703
root <root@demo.zabbix.demo>
2022-07-11 13:06:43 +0300

    next

--- refresh_stock.sh
+++ refresh_stock.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # remove old dir. start fresh
-rm -rf /tmp/6.0.zip
-rm -rf /tmp/zabbix-release-6.0
+rm -rf /tmp/6.2.zip
+rm -rf /tmp/zabbix-release-6.2
 
 # generate a SID by using:
 # "Administration" => "General" => "API tokens"
@@ -11,18 +11,18 @@ SID=$(cat ~/.zabbixapi)
 # API endpoint
 JSONRPC=http://demo.zabbix.demo/api_jsonrpc.php
 
-# download latest 6.0 branch
-curl -kL https://github.com/zabbix/zabbix/archive/refs/heads/release/6.0.zip -o /tmp/6.0.zip
+# download latest 6.2 branch
+curl -kL https://github.com/zabbix/zabbix/archive/refs/heads/release/6.2.zip -o /tmp/6.2.zip
 
 # unzip
 cd /tmp
-unzip 6.0.zip
+unzip 6.2.zip
 
 # go back to previous directory where PHP program is located
 cd -
 
 # start template import
-find /tmp/zabbix-release-6.0/templates -type f -name '*.yaml' | \
+find /tmp/zabbix-release-6.2/templates -type f -name '*.yaml' | \
 while IFS= read -r TEMPLATE
 do {
 php delete_missing.php $SID $JSONRPC $TEMPLATE | jq .result | grep "true"
@@ -30,7 +30,7 @@ php delete_missing.php $SID $JSONRPC $TEMPLATE | jq .result | grep "true"
 [[ $? -ne 0 ]] && echo $TEMPLATE
 } done
 
-find /tmp/zabbix-release-6.0/templates/media -type f -name '*.yaml' | \
+find /tmp/zabbix-release-6.2/templates/media -type f -name '*.yaml' | \
 while IFS= read -r MEDIA
 do {
 php media_type.php $SID $JSONRPC $MEDIA | jq .result | grep "true"
